76b2a890045332fab40173c012044a1b811db8f7
Dima K <dima1086@hotmail.com>
2017-11-19 23:04:09 -0500

    Maps API for 3 cities.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
+var map;
+
 function initMap() {
+
     var toronto = {
-        lat: 43.653226,
-        lng: -79.383184
+        "lat": 43.653226,
+        "lng": -79.3831843
     };
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: toronto
     });
@@ -11,4 +14,41 @@ function initMap() {
         position: toronto,
         map: map
     });
+
+
+
+
 }
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            console.log(results[i]);
+            createMarker(results[i]);
+
+        }
+    }
+}
+
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        position: place.geometry.location,
+        title: place.name,
+        animation: google.maps.Animation.DROP,
+        map: map
+    });
+}
+
+$('.dropdown-menu .dropdown-item').click(function() {
+    var city = $(this).attr("value");
+    console.log(city);
+
+    var request = {
+        query: city + "+point+of+interest",
+        language: "en"
+    }
+
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+})
